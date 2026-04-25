5f28ce26e4f668fe11aa773236541ed3aa71c177
nahidAi <34857951+nahidAi@users.noreply.github.com>
2026-04-25 14:11:12 +0330

    Update recommend.js

--- api/recommend.js
+++ api/recommend.js
@@ -21,7 +21,7 @@ No additional text.
 `;
 
     const response = await fetch(
-      "https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=" +
+      "https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=" +
         process.env.GEMINI_API_KEY,
       {
         method: "POST",
