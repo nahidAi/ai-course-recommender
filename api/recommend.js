7b3fc2e8384def1bc592cb7cc33cfcd816e25f2c
nahidAi <34857951+nahidAi@users.noreply.github.com>
2026-04-27 19:19:07 +0330

    Update recommend_2.js

--- api/recommend.js
+++ api/recommend.js
@@ -8,7 +8,8 @@ module.exports = async function handler(req, res) {
 
   try {
     const response = await fetch(
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=" + apiKey,
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent"
+" + apiKey,
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
